04f246c0734d7b201d01113305cfce0fc6d00b4e
Peter <peter.m.farmer.204@gmail.com>
2023-08-10 17:46:37 +1000

    task 4 code complete

--- scenarioCalculator.py
+++ scenarioCalculator.py
@@ -4,14 +4,14 @@ class Calculator:
 
     def calculateScenario(self, x, y):
         correctAnswer = self.calculate('-', '*', x, y)
-        print(f"CorrectAnswer = {correctAnswer}")
         for symbolOne in self.possibilities:
             for symbolTwo in self.possibilities:
                 if symbolOne == '-' and symbolTwo == '*':
                     continue
                 result = self.calculate(symbolOne, symbolTwo, x, y)
-                print(result, symbolOne, symbolTwo)
                 if result == correctAnswer:
+                    print(f"CorrectAnswer = {correctAnswer}")
+                    print(result, symbolOne, symbolTwo)
                     return False
 
         return True
@@ -40,8 +40,21 @@ class Calculator:
         else:
             return x / 2
 
+    def calculateBadSolutionsInRange(self, absVal):
+        erroneous = []
+        for n in range(-absVal, absVal):
+            if not self.calculateScenario(n, 1):
+                erroneous.append(n)
+
+        return erroneous
+
 
 print("Task 3 Start")
 task3Tester = Calculator()
 print(task3Tester.calculateScenario(2, 1))
 print("Task 3 end")
+
+print("task 4 start")
+task4Tester = Calculator()
+print(task4Tester.calculateBadSolutionsInRange(10000))
+print("task4End")
